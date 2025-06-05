c1df67b6ef59b9889cf20bdca709b317aa7735a6
Augustvilliam <willyp@hotmail.se>
2025-06-05 12:54:13 +0200

    Fixed broken constructor in controller.

--- Controllers/VerificationController.cs
+++ Controllers/VerificationController.cs
@@ -9,32 +9,38 @@ namespace EmailVerificationService.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
-public class VerificationController(IVerificationService verificationController) : ControllerBase
+public class VerificationController : ControllerBase
 {
-    private readonly IVerificationService _verificationService = verificationController;
+    private readonly IVerificationService _verificationService;
+
+    public VerificationController(IVerificationService verificationService)
+    {
+        _verificationService = verificationService;
+    }
+
     [AllowAnonymous]
     [HttpPost("send")]
-    public async Task<IActionResult> Send(SendVerificationCodeRequest request)
+    public async Task<IActionResult> Send([FromBody] SendVerificationCodeRequest request)
     {
         if (!ModelState.IsValid)
-            return BadRequest(new { Error = "Email Adress is required" });
+            return BadRequest(new { Error = "Email Address is required" });
 
         var result = await _verificationService.SendVerificationEmailAsync(request);
         return result.Succeeded
             ? Ok(result)
             : StatusCode(500, result);
     }
+
     [AllowAnonymous]
     [HttpPost("verify")]
-    public IActionResult Verify(VerifyVerificationCodeRequest request)
+    public IActionResult Verify([FromBody] VerifyVerificationCodeRequest request)
     {
         if (!ModelState.IsValid)
-            return BadRequest(new { Error = "Inavlid or expired Code" });
+            return BadRequest(new { Error = "Invalid or expired code" });
 
         var result = _verificationService.VerifyVerificationCode(request);
         return result.Succeeded
             ? Ok(result)
             : StatusCode(500, result);
-
     }
 }
